42e11766b825cfdb6fc65ae3a243c07bc4877f9e
explosion-mental <explosion0mental@gmail.com>
2023-03-25 23:41:19 -0500

    clean up code a bit, add more comments and references + some design choices

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,78 @@
 //use std::io::Cursor;
 use image::io::Reader as ImageReader;
-//use image::{GenericImage, GenericImageView, ImageBuffer, RgbImage};
 use std::collections::HashMap;
 
 //use colorsys::{ColorAlpha, Hsl, Rgb};
-use image::GenericImageView;
-use lab::Lab;
+use lab::{Lab,rgbs_to_labs};
 use owo_colors::*;
 
 //TODO use BTree from std instead of HashMap
+//TODO handle errors
+//TODO clap
+
+struct Histo {
+    value: Lab,
+    count: usize,
+}
+
+/// #definition thingy
+/// > The lightness value, L*, also referred to as "Lstar," defines black at 0 and white at 100. The a*
+/// > axis is relative to the green–red opponent colors, with negative values toward green and positive
+/// > values toward red. The b* axis represents the blue–yellow opponents, with negative numbers toward
+/// > blue and positive toward yellow.
+/// ref: <https://en.wikipedia.org/wiki/CIELAB_color_space>
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file = "./test.png";
-    let mut cols: HashMap<i32, (Lab, u32)> = HashMap::new();
-    //println!("{} and {}", file!(), module_path!());
 
-    let img = ImageReader::open(file)?.decode()?;
-    //let (rgbas, _) = match img.as_rgba8()  {
-    //    Some(s) => s,
-    //    None => panic!(),
-    //};
-    //let (a, b, image::Rgba(rgbas)) = img.pixels();
-    //let lab = Lab::rgbs_to_lab(rgbas);
-    //let mut prev: [u8; 4] = [0, 0, 0, 0];
-    let mut prev = Lab::default();
+    // Init image, then convert it into rgb and finally to LAB
+    let img = ImageReader::open(file)?.decode()?.to_rgba8();
+    let rgbs = img.as_raw();
+    let labs = lab::rgb_bytes_to_labs(rgbs);
+    //let rgbs = img.to_rgb8().get_pixel::<>();
 
-    //Iterate over all pixels in the image.
-    for (_, _, image::Rgba(a)) in img.pixels() {
-        let lab = Lab::from_rgba(&a);
-        let diff = delta_e(lab, prev);
-        println!("{}", diff);
-        //let sum: u32 = (lab.l + lab.a + lab.b) as u32;
-        match cols.get(&diff) {
-            Some(s) => {
-                let (_, count) = s;
-                cols.insert(diff, (lab, count + 1))
-            },
-            None => cols.insert(diff, (lab, 1)),
-        };
-        prev = lab;
-        //println!("R: {} | G: {} | B: {} | A: {}", a[0], a[1], a[2], a[3]);
-        //     // Do something with pixel.
-    }
+    //      Lab, count,
+    let mut histo: Vec<Histo> = vec![];
 
-    for (i, (colab, count)) in &cols {
-        let rgb = colab.to_rgb();
-        println!("{} and {:?} {} and hex: {:?}", i, colab, count, rgb.on_color(Rgb(rgb[0], rgb[1], rgb[2])));
+    for lab in labs {
+        if !is_present(lab, &mut histo) {
+            histo.push(Histo {value: lab, count: 1});
+        } else {
+            continue;
+        }
+
+        let a = lab.to_rgb();
+        print!("{}   ", "COLOR".on_color(Rgb(a[0], a[1], a[2])));
     }
 
     Ok(())
+}
+
+/// This should use delta_e somehow
+fn is_present(lab: Lab, v: &Vec<Histo>) -> bool {
+    let mut i = v.iter();
+    let found = i.any(|x| {
+        if x.value.a == lab.a
+        && x.value.b == lab.b {
+            return true;
+        }
+        false
+    });
+    found
 }
 
 /// This generates the differents between two CIELAB colors
 ///
 /// ref: <https://www.easyrgb.com/en/math.php>
 fn delta_e(current: Lab, previous: Lab) -> i32 {
-        let deltae: i32 = (
+        let deltae =
             (
-              ((previous.l - current.l) as i32 ^ 2)
-            + ((previous.a - current.a) as i32 ^ 2)
-            + ((previous.b - current.b) as i32 ^ 2)
-            )
-            as f32).sqrt() as i32;
+                (
+                        (((previous.l - current.l) as i32) ^ 2)
+                    +   (((previous.a - current.a) as i32) ^ 2)
+                    +   (((previous.b - current.b) as i32) ^ 2)
+                )
+            as f32).sqrt();
 
-        deltae
+        deltae as i32
 }
