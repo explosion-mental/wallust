796950372f1b2c740aebbf138825933b67d87714
explosion-mental <explosion0mental@gmail.com>
2023-03-28 14:47:10 -0500

    update cache functions

--- src/cache.rs
+++ src/cache.rs
@@ -1,5 +1,10 @@
 //! Cache functions, stored in json
+//!
+//! You either write_cache() or read_cache()
 use std::path::PathBuf;
+use std::fs;
+use std::os::unix::fs::MetadataExt;
+use std::time::SystemTime;
 
 use crate::Colors;
 use crate::MyLab;
@@ -10,13 +15,15 @@ use anyhow::Result;
 //TODO to overwrite or to check if it exist first? maybe a cli flag to overwrite/delete cache
 pub fn write_cache(colors: Colors<MyLab>, file: &PathBuf) -> Result<()> {
     if already_cached(file)? { return Ok(()); }
+    let cname = cachename(file);
     println!("{}", serde_json::to_string(&colors)?);
     Ok(())
 }
 
 /// Loads data from a cached file
-pub fn read_cache() -> Result<Colors<MyLab>> {
+pub fn read_cache(file: &PathBuf) -> Result<Colors<MyLab>> {
     let path = "~/.cache/wallust";
+    let cname = cachename(file);
     let contents = std::fs::read_to_string(path)?;
     Ok(serde_json::from_str(&contents)?)
 }
@@ -28,5 +35,16 @@ pub fn already_cached(f: &PathBuf) -> Result<bool> {
 
 /// Generates the name the cache file it's gonna use, which is a hash
 fn cachename(file: &PathBuf) -> Result<String> {
-    Ok("".into())
+    let md = fs::metadata(&file)?;
+    let birth = if let Ok(o) = md.created() { o } else { panic!("Not Supported") };
+    let modif = if let Ok(o) = md.modified() { o } else { panic!("Not Supported") };
+    Ok(
+    format!("{}{}{}{}{}",
+        file.display(),
+        md.ino(),
+        //md.file_type(),
+        md.len(),
+        birth.duration_since(SystemTime::UNIX_EPOCH)?.as_secs(),
+        modif.duration_since(SystemTime::UNIX_EPOCH)?.as_secs(),
+    ))
 }
