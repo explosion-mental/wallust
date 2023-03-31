293133639110a4d00708e0f37a116a90f6428771
explosion-mental <explosion0mental@gmail.com>
2023-03-31 14:16:15 -0500

    add wal backend, but don't expose it yet

---
+++ src/backends/wal.rs
@@ -0,0 +1,43 @@
+use crate::backends::*;
+use std::process::Command;
+use std::str;
+
+/// use Image Magick to get colors
+//TODO flatten the hues like pywal
+pub fn wal(f: &PathBuf, threshold: u32) -> Result<Colors> {
+
+    let im = Command::new("convert")
+        .arg(f)
+        .arg("-resize")
+        .arg("25%")
+        .arg("-colors")
+        .arg("9")
+        .arg("-unique-colors")
+        .arg("txt:-")
+        .output()?;
+
+    let mut cols: Vec<[u8; 3]> = vec![];
+    // The output is like the following:
+    //   0,0: (92,64,54)  #5C4036  srgb(36.1282%,25.1188%,21.1559%)
+    //            ^
+    //    we care bout this one
+    for line in str::from_utf8(&im.stdout)?.lines().skip(1) {
+        println!("{}", line);
+        let mut s = line.split_ascii_whitespace();
+        let Some(_) = s.next() else {
+            anyhow::bail!("convert failed giving colors");
+        };
+        let c = s.next().unwrap().replace(&['(', ')'], "");
+        let mut split = c.split(",");
+        let r = split.next().unwrap().parse::<u8>()?;
+        let g = split.next().unwrap().parse::<u8>()?;
+        let b = split.next().unwrap().parse::<u8>()?;
+        cols.push([r, g, b]);
+    }
+    println!("{:?}", cols);
+
+    let labs = lab::rgbs_to_labs(&cols);
+
+    let mut histo = gen_histogram(labs, threshold);
+    Ok(Colors::from(&mut histo))
+}
