b14aa5ec4d4dc2bc08cdc2618ec9a2bda35b8a14
explosion-mental <explosion0mental@gmail.com>
2023-04-06 22:15:00 -0500

    prefer to use str slice

--- src/backends/wal.rs
+++ src/backends/wal.rs
@@ -1,5 +1,6 @@
 use crate::backends::*;
 use std::process::Command;
+use std::str;
 
 /// use Image Magick to get colors
 //TODO flatten the hues like pywal
@@ -16,13 +17,11 @@ pub fn wal(f: &PathBuf) -> Result<Vec<u8>> {
         .output()?;
 
     let mut cols: Vec<u8> = vec![];
-    let output = im.stdout;
-
     // The output is like the following:
     //   0,0: (92,64,54)  #5C4036  srgb(36.1282%,25.1188%,21.1559%)
     //            ^
-    //    we care bout this representation
-    for line in String::from_utf8(output)?.lines().skip(1) {
+    //    we care bout this one
+    for line in str::from_utf8(&im.stdout)?.lines().skip(1) {
         let mut s = line.split_ascii_whitespace();
         let Some(_) = s.next() else {
             anyhow::bail!("convert failed giving colors");
