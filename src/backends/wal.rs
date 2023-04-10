a1ab39cb605bc86d70cf7a8e6297b956f88c2ffd
explosion-mental <explosion0mental@gmail.com>
2023-04-10 16:45:25 -0500

    comment wal.rs

--- src/backends/wal.rs
+++ src/backends/wal.rs
@@ -1,3 +1,14 @@
+//! # Wal
+//! * Uses image magick to generate the colors
+//! * We parse the hex string because the tuples seems to change, like if there are no green and
+//!   blue values and only red, the output would be like `(238)`, instead of `(238, 0, 0)`
+//! ## Sample output of `convert` is like the following:
+//! ```
+//!   0,0: (92,64,54)  #5C4036  srgb(36.1282%,25.1188%,21.1559%)
+//!   skip   skip         ^
+//!                we care bout this one
+//! ```
+
 use crate::backends::*;
 use std::process::Command;
 use std::str;
@@ -17,11 +28,6 @@ pub fn wal(f: &PathBuf) -> Result<Vec<u8>> {
         .output()?;
 
     let mut cols: Vec<u8> = vec![];
-
-    // Sample output of `convert` is like the following:
-    //   0,0: (92,64,54)  #5C4036  srgb(36.1282%,25.1188%,21.1559%)
-    //   skip     skip       ^
-    //              we care bout this one
 
     for line in str::from_utf8(&im.stdout)?.lines().skip(1) {
         let mut s = line.split_ascii_whitespace().skip(2);
