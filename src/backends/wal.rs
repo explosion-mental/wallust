630972862cca392d4c716963fea2193d3edf88e4
explosion-mental <explosion0mental@gmail.com>
2023-04-10 14:48:55 -0500

    change unwraps() for expects

--- src/backends/wal.rs
+++ src/backends/wal.rs
@@ -17,20 +17,22 @@ pub fn wal(f: &PathBuf) -> Result<Vec<u8>> {
         .output()?;
 
     let mut cols: Vec<u8> = vec![];
-    // The output is like the following:
+
+    // Sample output of `convert` is like the following:
     //   0,0: (92,64,54)  #5C4036  srgb(36.1282%,25.1188%,21.1559%)
     //            ^
     //    we care bout this one
+
     for line in str::from_utf8(&im.stdout)?.lines().skip(1) {
-        let mut s = line.split_ascii_whitespace();
-        let Some(_) = s.next() else {
-            anyhow::bail!("convert failed giving colors");
-        };
-        let c = s.next().unwrap().replace(['(', ')'], "");
+        let mut s = line.split_ascii_whitespace().skip(1);
+
+        let c = s.next().expect("Should be present as e.g. (0, 0, 0)").replace(['(', ')'], "");
         let mut split = c.split(',');
-        let r = split.next().unwrap().parse::<u8>()?;
-        let g = split.next().unwrap().parse::<u8>()?;
-        let b = split.next().unwrap().parse::<u8>()?;
+
+        let r = split.next().expect("convert outputs this").parse::<u8>()?;
+        let g = split.next().expect("convert outputs this").parse::<u8>()?;
+        let b = split.next().expect("convert outputs this").parse::<u8>()?;
+
         cols.push(r);
         cols.push(g);
         cols.push(b);
