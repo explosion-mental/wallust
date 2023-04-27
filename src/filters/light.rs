2065c786b5c715cbf3ab0939c4dc0f61bbb69740
explosion-mental <explosion0mental@gmail.com>
2023-04-27 11:58:15 -0500

    add light scheme

---
+++ src/filters/light.rs
@@ -0,0 +1,66 @@
+//! Default method to generate colors
+use crate::filters::*;
+
+pub fn light(coo: Vec<Myrgb>) -> Colors {
+    let mut c = coo.clone();
+    let len = c.len();
+
+    // This method requires at least 6 colors, if not, generate new ones
+    //TODO generate new colors by mixing (and maybe tweak by some random number)
+    if len < 6 {
+        eprintln!("Not enought colors! Generating new colors from fetches ones...");
+        for (i, value) in coo.iter().enumerate() {
+            // generate new colors, but switch between the how (method to use)
+            let val = if i % 2 == 0 {
+                value.darken(0.5)
+            } else {
+                value.lighten(0.5)
+            };
+            c.push(val);
+        }
+    }
+
+
+        //raw_colors[0] = util.lighten_color(colors[-1], 0.85)
+        //raw_colors[7] = colors[0]
+        //raw_colors[8] = util.darken_color(colors[-1], 0.4)
+        //raw_colors[15] = colors[0]
+
+
+    // This parser only needs 6 colors [0..=5]
+    let lightest = c.last().expect("not empty");
+    let darkest = c.first().expect("not empty");
+
+    let bg = lightest.lighten(0.85);
+    let fg = darkest.darken(0.55);
+
+    let col7  = darkest.darken(0.55);
+    let col15 = darkest.darken(0.85);
+
+    let col8  = lightest.darken(0.3); //color 8 needs to be a bit brighter to contrast color0 and background
+
+    Colors {
+        background : bg, // background
+        foreground : fg,
+
+        /* First row */
+        color0 : *lightest, // background
+        color1 : c[6],
+        color2 : c[5],
+        color3 : c[4],
+        color4 : c[3],
+        color5 : c[2],
+        color6 : c[1],
+        color7 : col7, // fg
+
+        /* Second row */
+        color8 : col8, // darker than col0
+        color9 : c[6],
+        color10: c[5],
+        color11: c[4],
+        color12: c[3],
+        color13: c[2],
+        color14: c[1],
+        color15: col15, //a little darken than col7
+    }
+}
