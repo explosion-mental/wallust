0335b51ce1a0ddba82ef0f07ed6c39a616f0210d
explosion-mental <explosion0mental@gmail.com>
2023-11-13 22:27:37 -0500

    filters: harddark, light16: reuse existing code to avoid code duplication

--- src/filters/harddark.rs
+++ src/filters/harddark.rs
@@ -4,46 +4,25 @@ use crate::filters::*;
 /// dark background, light foreground, with dark hard hued colors.
 /// Sorted by [`DarkFirst`]
 pub fn harddark(c: &[Myrgb]) -> Colors {
-    let ee = Myrgb(238, 238, 238); //This is `#EEEEEE`
+    //scheme
+    let mut s = super::dark::dark(c);
 
-    // This parser only needs 6 colors [0..=5]
-    let lightest = c.last().expect("not empty");
-    let darkest = c.first().expect("not empty");
+    // invert the order of the colors
+    /* first row */
+    s.color1 = c[0];
+    s.color2 = c[1];
+    s.color3 = c[2];
+    s.color4 = c[3];
+    s.color5 = c[4];
+    s.color6 = c[5];
 
-    let bg = darkest.darken(0.55);
-    let fg = lightest.lighten(0.85);
+    /* second row */
+    s.color9 = c[0];
+    s.color10= c[1];
+    s.color11= c[2];
+    s.color12= c[3];
+    s.color13= c[4];
+    s.color14= c[5];
 
-    let col7  = lightest.blend(ee);
-    let col15 = lightest.blend(ee).darken(0.2);
-
-    let col8  = col7.darken(0.30); //color 8 needs to be a bit brighter to contrast color0 and background
-
-    // darken the background color slightly, just like pywal
-    let f = format!("{:02x}", darkest.0).chars().last().expect("garanted to have 2 elements by the fmt");
-    let col0  = if f != '0' { bg } else { darkest.darken(0.4) };
-
-    Colors {
-        background : bg, // background
-        foreground : fg,
-
-        /* First row */
-        color0 : col0, // background
-        color1 : c[0],
-        color2 : c[1],
-        color3 : c[2],
-        color4 : c[3],
-        color5 : c[4],
-        color6 : c[5],
-        color7 : col7, // fg
-
-        /* Second row */
-        color8 : col8, // darker than col0
-        color9 : c[0],
-        color10: c[1],
-        color11: c[2],
-        color12: c[3],
-        color13: c[4],
-        color14: c[5],
-        color15: col15, //a little darken than col7
-    }
+    s
 }
--- src/filters/light16.rs
+++ src/filters/light16.rs
@@ -4,40 +4,15 @@ use crate::filters::*;
 /// Variation of the [`light`] scheme, but with a 16 variation, similar to how [`dark16`] does it.
 /// Sorted by [`DarkFirst`]
 pub fn light16(c: &[Myrgb]) -> Colors {
-    // This parser only needs 6 colors [0..=5]
-    let lightest = c.last().expect("not empty");
-    let darkest = c.first().expect("not empty");
+    let mut c = super::light::light(c);
 
-    let bg = lightest.lighten(0.85);
-    let fg = darkest.darken(0.55);
+    /* First row */
+    c.color1 = c.color1.darken(0.25);
+    c.color2 = c.color2.darken(0.25);
+    c.color3 = c.color3.darken(0.25);
+    c.color4 = c.color4.darken(0.25);
+    c.color5 = c.color5.darken(0.25);
+    c.color6 = c.color6.darken(0.25);
 
-    let col7  = darkest.darken(0.55);
-    let col15 = darkest.darken(0.85);
-
-    let col8  = lightest.darken(0.3); //color 8 needs to be a bit brighter to contrast color0 and background
-
-    Colors {
-        background : bg, // background
-        foreground : fg,
-
-        /* First row */
-        color0 : *lightest, // background
-        color1 : c[5].darken(0.25),
-        color2 : c[4].darken(0.25),
-        color3 : c[3].darken(0.25),
-        color4 : c[2].darken(0.25),
-        color5 : c[1].darken(0.25),
-        color6 : c[0].darken(0.25),
-        color7 : col7, // fg
-
-        /* Second row */
-        color8 : col8, // darker than col0
-        color9 : c[5],
-        color10: c[4],
-        color11: c[3],
-        color12: c[2],
-        color13: c[1],
-        color14: c[0],
-        color15: col15, //a little darken than col7
-    }
+    c
 }
