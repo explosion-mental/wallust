d91e45dd055d11e054904c239013c4bb295de452
explosion-mental <explosion0mental@gmail.com>
2023-12-03 00:34:36 -0500

    filters: - softlight: use light() function to fill bg, fg, col0 and col7 - harddark: use the `Struct { field1, ..func() }` to fill `Colors`

--- src/filters/harddark.rs
+++ src/filters/harddark.rs
@@ -4,25 +4,22 @@ use crate::filters::*;
 /// dark background, light foreground, with dark hard hued colors.
 /// Sorted by [`DarkFirst`]
 pub fn harddark(c: &[Myrgb]) -> Colors {
-    //scheme
-    let mut s = super::dark::dark(c);
+    Colors {
+        /* First row */
+        color1 : c[0],
+        color2 : c[1],
+        color3 : c[2],
+        color4 : c[3],
+        color5 : c[4],
+        color6 : c[5],
 
-    // invert the order of the colors
-    /* first row */
-    s.color1 = c[0];
-    s.color2 = c[1];
-    s.color3 = c[2];
-    s.color4 = c[3];
-    s.color5 = c[4];
-    s.color6 = c[5];
-
-    /* second row */
-    s.color9 = c[0];
-    s.color10= c[1];
-    s.color11= c[2];
-    s.color12= c[3];
-    s.color13= c[4];
-    s.color14= c[5];
-
-    s
+        /* Second row */
+        color9 : c[0],
+        color10: c[1],
+        color11: c[2],
+        color12: c[3],
+        color13: c[4],
+        color14: c[5],
+        ..super::dark::dark(c)
+    }
 }
