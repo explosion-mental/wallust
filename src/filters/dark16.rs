87334ea4c55a384e2b5be9207804ac39b9d9f991
explosion-mental <explosion0mental@gmail.com>
2023-12-05 21:10:23 -0500

    filters: 16 variations, just use the method for it

--- src/filters/dark16.rs
+++ src/filters/dark16.rs
@@ -7,14 +7,5 @@ use crate::filters::*;
 /// Sorted by [`LightFirst`]
 /// Variation of dark with 16 colors. From <https://github.com/eylles/pywal16>
 pub fn dark16(c: &[Myrgb]) -> Colors {
-    let mut c = super::dark::dark(c);
-
-    c.color1 = c.color1.darken(0.25);
-    c.color2 = c.color2.darken(0.25);
-    c.color3 = c.color3.darken(0.25);
-    c.color4 = c.color4.darken(0.25);
-    c.color5 = c.color5.darken(0.25);
-    c.color6 = c.color6.darken(0.25);
-
-    c
+    super::dark::dark(c).to_16col()
 }
--- src/filters/light16.rs
+++ src/filters/light16.rs
@@ -4,15 +4,5 @@ use crate::filters::*;
 /// Variation of the [`light`] scheme, but with a 16 variation, similar to how [`dark16`] does it.
 /// Sorted by [`DarkFirst`]
 pub fn light16(c: &[Myrgb]) -> Colors {
-    let mut c = super::light::light(c);
-
-    /* First row */
-    c.color1 = c.color1.darken(0.25);
-    c.color2 = c.color2.darken(0.25);
-    c.color3 = c.color3.darken(0.25);
-    c.color4 = c.color4.darken(0.25);
-    c.color5 = c.color5.darken(0.25);
-    c.color6 = c.color6.darken(0.25);
-
-    c
+    super::light::light(c).to_16col()
 }
