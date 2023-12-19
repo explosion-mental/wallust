62885455c0e034c3c0765bdecb085b28128344f0
explosion-mental <explosion0mental@gmail.com>
2023-12-19 18:50:24 -0500

    filters: resized: while shrinking, respect aspect ratio

--- src/backends/resized.rs
+++ src/backends/resized.rs
@@ -3,10 +3,19 @@ use crate::backends::*;
 /// Resize it, then get read the image
 pub fn resized(f: &Path) -> Result<Vec<u8>> {
     let (true_w, true_h) = image::image_dimensions(f)?;
-    let shrink = |x| if x > 512 { x / 4 } else { x };
-    let w = shrink(true_w);
-    let h = shrink(true_h);
+
+    let (w, h) = shrink(true_w, true_h);
     let img = image::open(f)?.resize(w, h, image::imageops::Gaussian);
     Ok(img.into_rgb8().into_raw())
 }
 
+/// Calculates the new resized sizes, **keeping the aspect ratio**.
+fn shrink(w: u32, h: u32) -> (u32, u32) {
+    let resized = if w >= 1024 || h >= 1024 { true } else { false };
+
+    if resized {
+        (w / 2, h / 2)
+    } else {
+        (w, h)
+    }
+}
