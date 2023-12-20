be1604ba2c74ec966b800700dc7f9c5c5448cb85
explosion-mental <explosion0mental@gmail.com>
2023-12-19 22:59:13 -0500

    _idea: new crate?

--- tests/config.rs
+++ tests/config.rs
@@ -1,5 +1,6 @@
 /// testing for the validity of the config file is done in `args.rs`
 /// a current hack to avoid build.rs hell (requires dividing types into a crate itself or include! hacks)
+/// XXX this seems so useful, maybe elaborate this idea into it's own crate.
 #[cfg(feature = "makeconfig")]
 #[test]
 fn main() {
