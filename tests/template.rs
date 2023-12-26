c1647b083f038f5ad68082758a9ef3d15b996715
explosion-mental <explosion0mental@gmail.com>
2023-12-26 00:46:05 -0500

    tests: template: also tests `new_engine` syntax

--- tests/template.rs
+++ tests/template.rs
@@ -9,6 +9,8 @@ use wallust::template;
 use wallust::config;
 //use wallust::colors;
 
+//TODO add tests for every KEY combination
+
 const COLS: &Colors = &Colors {
         background: Myrgb(238, 238, 238), //#EEEEEE
         foreground: Myrgb(221, 221, 221), //#DDDDDD
@@ -107,6 +109,100 @@ color15='#0F0000'
             template: template_path.display().to_string(),
             target: target_path.display().to_string(),
             new_engine: Some(false),
+        }]),
+        ..config::Config::default()
+    };
+
+    let e = c.entry.as_ref().unwrap();
+
+    template::write_template(&c, Path::new(""), &e, COLS, true).expect("should parse correctly");
+
+    //store templated string
+    let target_content = std::fs::read_to_string(&e[0].target).expect("TARGET CONTENT");
+
+    // templated file should be the same as expected in the target_sample
+    assert_eq!(target_content, target_sample);
+
+    // `rm`ing the temporal dir will also close the files inside it
+    tmpdir.close().expect("temporal directory should close successfully");
+}
+
+/// Like the above but with the new engine
+#[test]
+fn variables_new_engine() {
+    let template_sample =
+r#"
+# Special
+wallpaper="{{wallpaper}}"
+background='{{background}}'
+foreground='{{foreground}}'
+cursor='{{cursor}}'
+
+# Colors
+color0='{{color0}}'
+color1='{{color1}}'
+color2='{{color2}}'
+color3='{{color3}}'
+color4='{{color4}}'
+color5='{{color5}}'
+color6='{{color6}}'
+color7='{{color7}}'
+color8='{{color8}}'
+color9='{{color9}}'
+color10='{{color10}}'
+color11='{{color11}}'
+color12='{{color12}}'
+color13='{{color13}}'
+color14='{{color14}}'
+color15='{{color15}}'
+"#;
+
+    let target_sample =
+r#"
+# Special
+wallpaper=""
+background='#EEEEEE'
+foreground='#DDDDDD'
+cursor='#DDDDDD'
+
+# Colors
+color0='#000000'
+color1='#010000'
+color2='#020000'
+color3='#030000'
+color4='#040000'
+color5='#050000'
+color6='#060000'
+color7='#070000'
+color8='#080000'
+color9='#090000'
+color10='#0A0000'
+color11='#0B0000'
+color12='#0C0000'
+color13='#0D0000'
+color14='#0E0000'
+color15='#0F0000'
+"#;
+
+    let tmpdir = tempfile::tempdir().expect("init new temporal named pipe");
+
+    // file in which the template variables are
+    let template_path = tmpdir.path().join("colors-template.sh");
+    let mut template = File::create(&template_path).expect("should created a tmp file");
+    write!(template, "{template_sample}").expect("should write to tmp correctly");
+
+    // file in which, after templating, it's gonna be writeable
+    let target_path = tmpdir.path().join("colors.sh");
+    let mut _target = File::create(&target_path).expect("should created a tmp file");
+
+    // usual config
+    let c = config::Config {
+        check_contrast: None,
+        dir: tmpdir.path().into(),
+        entry: Some(vec![config::Entries {
+            template: template_path.display().to_string(),
+            target: target_path.display().to_string(),
+            new_engine: Some(true),
         }]),
         ..config::Config::default()
     };
