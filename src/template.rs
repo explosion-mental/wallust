4ff7283c80d310be18ee941fbbd12b5150a45274
explosion-mental <explosion0mental@gmail.com>
2023-12-30 12:19:53 -0500

    template: implement alpha_hex - from #23

--- src/template.rs
+++ src/template.rs
@@ -92,6 +92,7 @@ impl<'a> far::Render for Myt<'a> {
             "wallpaper",
             "alpha",
             "alpha_dec",
+            "alpha_hex",
             "backend",
             "colorspace",
             "filter",
@@ -255,6 +256,9 @@ impl<'a> far::Render for Myt<'a> {
 pub fn to_hash<'a>(col: &Colors, image_path: &Path, conf: &Config) -> HashMap<&'a str, String> {
     let mut map = HashMap::new();
     let alpha = conf.alpha.unwrap_or(100);
+    // list of hexadecimal alpha values https://gist.github.com/lopspower/03fb1cc0ac9f32ef38f4
+    let alphas_dec = [ "00", "03", "05", "08", "0A", "0D", "0F", "12", "14", "17", "1A", "1C", "1F", "21", "24", "26", "29", "2B", "2E", "30", "33", "36", "38", "3B", "3D", "40", "42", "45", "47", "4A", "4D", "4F", "52", "54", "57", "59", "5C", "5E", "61", "63", "66", "69", "6B", "6E", "70", "73", "75", "78", "7A", "7D", "80", "82", "85", "87", "8A", "8C", "8F", "91", "94", "96", "99", "9C", "9E", "A1", "A3", "A6", "A8", "AB", "AD", "B0", "B3", "B5", "B8", "BA", "BD", "BF", "C2", "C4", "C7", "C9", "CC", "CF", "D1", "D4", "D6", "D9", "DB", "DE", "E0", "E3", "E6", "E8", "EB", "ED", "F0", "F2", "F5", "F7", "FA", "FC", "FF", ];
+
     // make sure to display the absolute path of the wallpaper
     let p = std::fs::canonicalize(image_path).expect("PATH EXIST, validation from clap");
 
@@ -264,7 +268,7 @@ pub fn to_hash<'a>(col: &Colors, image_path: &Path, conf: &Config) -> HashMap<&'
     map.insert("wallpaper", p.display().to_string());
     map.insert("alpha", alpha.to_string());
     map.insert("alpha_dec", format!("{:.2}", f32::from(alpha) / 100.0 ));
-    //map.insert("alpha_hex", format!("{:.02x}",  ((f32::from(alpha) / 100.0)  * 255.0) as i32 ));
+    map.insert("alpha_hex", alphas_dec.get(alpha as usize).expect("CANNOT OVERFLOW, validation with clap 0..=100").to_string());
 
     // Include backend, colorspace and filter
     map.insert("backend", conf.backend.to_string());
