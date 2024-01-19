0ff03fd004e6c214b97eabf45fe98771a2e18004
explosion-mental <explosion0mental@gmail.com>
2024-01-18 22:38:45 -0500

    filters: softdark16: make color15 brighter, for better contrast

--- src/filters/softdark16.rs
+++ src/filters/softdark16.rs
@@ -2,5 +2,7 @@ use crate::filters::*;
 
 /// Variant of softlight with 16 colors
 pub fn softdark16(c: Cols) -> Colors {
-    super::softdark::softdark(c).to_16col()
+    let mut ret = softdark::softdark(c).to_16col();
+    ret.color15 = ret.color15.lighten(0.5);
+    ret
 }
