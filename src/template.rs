5695d020abbb5027638c035c90b42b80c65269dd
explosion-mental <explosion0mental@gmail.com>
2024-02-07 18:28:04 -0500

    template: prototype for the new pywal engine

--- src/template.rs
+++ src/template.rs
@@ -1,3 +1,4 @@
+#![allow(unused)]
 //! Template stuff, definitions and how it's parsed
 use std::fs::read_to_string;
 use std::fs::File;
@@ -8,9 +9,109 @@ use std::collections::HashMap;
 use crate::config::Config;
 use crate::config::Entries;
 use crate::colors::Colors;
+use crate::colors::Myrgb;
 
 use anyhow::Result;
 use owo_colors::OwoColorize;
+
+use fancy_regex::Regex;
+//use fancy_regex::Matches;
+
+/// This is a `pywal` template wannabe
+/// <https://github.com/dylanaraps/pywal/wiki/User-Template-Files>
+/// Read template file, substitute markers and save the file elsewhere.
+/// TODO improve this engine, currently I'm following the pywal structure
+/// (like reading line by line), however, this is rust. We can do better (and faster :p)
+/// TOOD no need to read line by line
+/// TODO escaping incrementanly brackets by using a recursive pure function
+/// (or ignore it and hardcode 3 escapes in the meantime) (also there can be `.chars()` method in use)
+fn template(all: &Myt, template_data: &String) -> Result<String> {
+    let colors = all.cols;
+    //let template_data = std::fs::read_to_string(&file).unwrap();
+    // this regex is insane, very well done dylan (pywal)
+    //
+    // Yet not that necessary? /{([^}]*)}/ or /{(.*?)}/ seems to also werk fine AND doesnt match after new lines
+    let re = Regex::new(r"(?<=(?<!\{))(\{([^{}]+)\})(?=(?!\}))").unwrap();
+    let hash = to_hash(all.cols, all.img, all.conf);
+
+    //for compatibility reasons, in pywal there was a need to use `{{` and `}}` to escape `{` and `}`
+    // The regex above will avoid things like `{{variable}}`, so there is no need for escaping
+    //let re = Regex::new(r"{{|}}").unwrap();
+
+    let mut ret = String::new();
+    for (i, l) in template_data.lines().enumerate() {
+        //println!("{i}| {l}");
+        // this means the for loop won't run
+        if ! re.is_match(l)? {
+            ret.push_str(l);
+        }
+        //get matches as an iterator
+        for mymatch in re.captures_iter(l) {
+            // ignore result for now
+            let m = mymatch?;
+            //println!("{m:?}");
+
+            //get the complete string, e.g. "wallpaper"
+            let mut name = match m.get(2) {
+                Some(s) => s.as_str(),
+                None => {
+                    eprintln!("Some error with '{m:?}'");
+                    continue;
+                }
+            };
+
+            let key = match hash.get(&name) {
+                Some(s) => s,
+                None => {
+                    eprintln!("Error: key '{name}' is not a valid variable.");
+                    continue;
+                }
+            };
+
+            //println!("name = {name}.{funcs} -> {new}");
+            let new = re.replace(l, key);
+            ret.push_str(&new);
+            //println!("{i}| {key}");
+
+            // XXX no need to split anything since we already have a hashmap with methods included.
+            // key has a method to be aplied to, figure out which one is it
+            //if name.contains(".") { //using a function
+
+                // XXX no need to split anything since we already have a hashmap with methods included.
+                // // split by key and method
+                // let groups = name.split(".").collect::<Vec<_>>();
+                // let name  = groups[0];
+                // let funcs = groups[1];
+                //
+                // if groups.len() > 2 { panic!("More than one dot") }
+                //
+                // //first get the hash key (which is a Myrgb)
+                // // then apply the method (funcs)
+                // // and finally re.replace
+
+                // let new = replace(colors, name, Some(funcs));
+                // println!("name = {name}.{funcs} -> {new}");
+                // ret.push_str(&new);
+                // re.replace(l, value);
+                // println!("{i}| {new}");
+            //} else {
+                // usual key
+
+                // let new = replace(colors, name, None);
+                // //println!("name = {name} -> {new}");
+                // ret.push_str(&new);
+                // println!("{i}| {new}");
+            //}
+
+            //println!("{name:?}");
+        }
+        ret.push('\n');
+    }
+    Ok(
+    ret
+    )
+}
+
 
 /// Writes `template`s into `target`s. Given the many possibilities of I/O errors, template errors,
 /// user typos, etc. Most errors are reported to stderr, and ignored to `continue` with the other
@@ -55,14 +156,17 @@ pub fn write_template(conf: &Config, image_path: &str, entries: &[Entries], valu
         let rendered = if e.new_engine.unwrap_or(false) {
             far::find_with_mode(file_content, far::Mode::AllowMissing)?.replace(&Myt { cols: values, img: image_path, conf })
         } else {
-            match new_string_template::template::Template::new(file_content).render(&values.to_hash(image_path, conf)) {
-                Ok(o) => o,
-                Err(er) => {
-                    let raw = r#"{{variable}}"#;
-                    eprintln!("[{warn}] File '{}': {er}\n[{warn}] Try using `new_engine = true` which changes syntax to double brackets '{raw}'", e.template);
-                    continue;
-                }
-            }
+            template(&Myt { cols: values, img: image_path, conf }, &file_content)?
+            // .with_regex(regex::Regex::new() doesn't seem to work neither with r"/{(.*?)}/" or r"/{([^}]*)}/"
+            // so I'm probably rolling my own pywal template implementation (see template())
+            // match new_string_template::template::Template::new(file_content).render(&values.to_hash(image_path, conf)) {
+            //     Ok(o) => o,
+            //     Err(er) => {
+            //         let raw = r#"{{variable}}"#;
+            //         eprintln!("[{warn}] File '{}': {er}\n[{warn}] Try using `new_engine = true` which changes syntax to double brackets '{raw}'", e.template);
+            //         continue;
+            //     }
+            // }
         };
 
         let mut buffer = match File::create(target_file.as_ref()) {
