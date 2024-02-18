492dbf4ddb2d60b5b39ffde2c3b69ede34c19580
explosion-mental <explosion0mental@gmail.com>
2024-02-18 18:50:29 -0500

    benches: require `iter` feature and refactor some of it's code

--- benches/backends.rs
+++ benches/backends.rs
@@ -1,49 +1,30 @@
-use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId};
-use wallust::backends::{self, Backend};
-use std::path::Path;
+use criterion::{criterion_group, criterion_main, Criterion};
 
-const SRC: [&str; 4] = [
-    "pexels-photo-356036.jpeg",
-    "pexels-photo-1146708.jpeg",
-    "pexels-photo-1567069.jpeg",
-    "pexels-photo-1089194.jpeg",
-];
+use wallust::backends::{self, Backend};
+
+use strum::IntoEnumIterator;
+
+include!("util.in");
 
 fn backends(c: &mut Criterion) {
-    let mut group = c.benchmark_group("backends");
+    let root = get_dir();
 
-    let home = std::process::Command::new("cargo").arg("locate-project").output().unwrap();
-    let home = std::str::from_utf8(&home.stdout).unwrap();
-    let root: serde_json::Value = serde_json::from_str(home).unwrap();
-    let root = root.pointer("/root").unwrap();
-    let root: String = serde_json::from_value(root.clone()).unwrap();
-    let root = &root[0..root.len() - "Cargo.toml".len()];
+    for curr in Backend::iter() {
+        let mut group = c.benchmark_group(curr.to_string());
 
-    let possible_cases = [
-        Backend::Full,
-        Backend::Resized,
-        Backend::Thumb,
-        Backend::Wal,
-        Backend::FastResize,
-    ];
+        //iterate over all images
+        for image in IMAGES {
+            let path = root.join("target").join("benchimg").join(image);
 
-    //iterate over all images
-    for i in SRC {
-        let name = i;
-        let i = &Path::new(&root).join("target").join("benchimg").join(i);
-
-        //with all possible backends
-        for j in possible_cases {
-            group.bench_with_input(
-                BenchmarkId::new(j.to_string(), name),
-                i,
-                |b, i| b.iter(|| backends::main(&j)(i).expect("Download the images"))
+            //with all possible backends
+            group.bench_function(
+                image,
+                |b| b.iter(|| backends::main(&curr)(&path).expect("Download the images"))
 
             );
         }
     }
 
-    group.finish();
 }
 
 criterion_group! {
--- benches/colorspaces.rs
+++ benches/colorspaces.rs
@@ -3,52 +3,29 @@ use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId};
 use wallust::backends::{self, Backend};
 use wallust::colorspaces::{self, ColorSpace};
 
-use std::path::Path;
+use strum::IntoEnumIterator;
 
-const SRC: [&str; 4] = [
-    "pexels-photo-356036.jpeg",
-    "pexels-photo-1146708.jpeg",
-    "pexels-photo-1567069.jpeg",
-    "pexels-photo-1089194.jpeg",
-];
+include!("util.in");
 
 fn colorspaces(c: &mut Criterion) {
-    let mut group = c.benchmark_group("color-spaces");
-
-    let home = std::process::Command::new("cargo").arg("locate-project").output().unwrap();
-    let home = std::str::from_utf8(&home.stdout).unwrap();
-    let root: serde_json::Value = serde_json::from_str(home).unwrap();
-    let root = root.pointer("/root").unwrap();
-    let root: String = serde_json::from_value(root.clone()).unwrap();
-    let root = &root[0..root.len() - "Cargo.toml".len()];
-
-    let possible_cases = [
-        ColorSpace::Lab,
-        ColorSpace::LabMixed,
-        ColorSpace::LabFast,
-    ];
-
+    let root = get_dir();
+    let path = root.join("target").join("benchimg");
     let threshold = 20;
 
-    for i in SRC {
-        let name = i;
+    let read_images = IMAGES.map(|x| backends::main(&Backend::Full)(&path.join(x)).unwrap());
 
-        println!("Reading image first.. {i}");
-        let p = &Path::new(&root).join("target").join("benchimg").join(i);
-        let sample = backends::main(&Backend::Resized)(Path::new(p)).unwrap_or_else(|_| panic!("Download the image {i}"));
-        println!("Done.\n");
+    for curr in ColorSpace::iter() {
+        let mut group = c.benchmark_group(curr.to_string());
 
-        for j in possible_cases {
+        for (idx, image) in read_images.iter().enumerate() {
             group.bench_with_input(
-                BenchmarkId::new(j.to_string(), name),
-                &sample,
-                |b, i| b.iter(|| colorspaces::main(j, i, threshold, &colorspaces::Generate::default()))
+                BenchmarkId::new(IMAGES[idx], image.len()),
+                &image,
+                |b, i| b.iter(|| colorspaces::main(curr, i, threshold, &colorspaces::Generate::default()))
 
             );
         }
     }
-
-    group.finish();
 }
 
 criterion_group! {
