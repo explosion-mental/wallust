3b28a741fc354f696782b3137169a50db09307cb
explosion-mental <explosion0mental@gmail.com>
2024-06-23 15:40:22 -0500

    palettes: softdark: make background a darker and "stronger" when needed

--- src/palettes/softdark.rs
+++ src/palettes/softdark.rs
@@ -7,13 +7,24 @@ use super::*;
 /// Modifies the background to match the most prominent color.
 /// Sorted by [`LightFirst`],
 pub fn softdark(c: Vec<Srgb>, orig: Vec<Srgb>) -> Colors {
+    use palette::IntoColor;
+    use palette::Saturate;
+
     //let orig = c.to_rgb_orig();
     let ee = Srgb::<u8>::new(238, 238, 238); //This is `#EEEEEE`
 
     //let bg = ret.foreground.darken(0.2);
     //let bg = orig[0].lighten(0.3);//.blend(ret.foreground);
-    let bg = Myrgb(orig[0]).saturate(0.2);
-    let bg: Srgb = bg.0;
+
+    // let bg = Myrgb(orig[0]).saturate(0.2);
+    // let bg: Srgb = bg.0;
+
+    let mut bg: palette::Hsl = orig[0].into_color();
+    //ensure the background is darken ENOUGH
+    if bg.lightness  > 0.5 { bg = bg.darken(0.5); }
+    if bg.saturation < 0.5 { bg = bg.saturate(0.15); }
+
+    let bg: Srgb = bg.into_color();
 
     let mut ret = softlight(c, orig);
     //lighten fg to maintain a good contrast and darken a bit the bg (super safe)
