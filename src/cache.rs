c0d81a5704a69c652e59e294106e423c6221a47c
explosion-mental <explosion0mental@gmail.com>
2024-10-08 18:58:45 -0500

    cache: Use cache filename instead of a directory hierachy, this is much cleaner given the hashing. - removes `inode` 'magic' number, given that we hash every byte, there   may be the same image with a different name (same contents in a   different file). - utilizes numbers, "magic numbers", instead of letters, given Windows   case insensitivity ;p - converts into base36 the hashed result, so it takes even less   characters.
    read_file_as_bytes -> fnv1a 32bit hash -> base36 u32

--- src/cache.rs
+++ src/cache.rs
@@ -6,16 +6,17 @@ use std::io::Write;
 use std::path::Path;
 use std::path::PathBuf;
 
-#[cfg(unix)]
-use std::os::unix::fs::MetadataExt;
-
-#[cfg(windows)]
-use std::os::windows::fs::MetadataExt;
-
 use crate::colors::Colors;
+use crate::backends::Backend;
+use crate::palettes::Palette;
+use crate::colorspaces::ColorSpace;
+use crate::colorspaces::FallbackGenerator;
 use crate::config::Config;
 
 use anyhow::{Result, Context};
+
+/// Cache versioning, to avoid breaks and missreadings.
+pub const CACHE_VER: &str = "1.4";
 
 /// Used to manage cache, rather than passing arguments in main() a lot
 #[derive(Debug, Default)]
@@ -35,94 +36,44 @@ impl fmt::Display for Cache {
     }
 }
 
-pub const CACHE_VER: &str = "1.4";
-
-/// Pretty fcking fast hashing
-/// the 32 bit version, should be enough for this use case
-/// Ref: https://en.m.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
-pub fn fnv1a(bytes: &[u8]) -> usize {
-    let mut hash = 2166136261;
-
-    for byte in bytes {
-        hash ^= *byte as usize;
-        hash = hash.wrapping_mul(16777619);
-    }
-
-    hash
-}
-
 impl Cache {
-    /// # Cache directory structure (using fs as a "database")
-    ///   1. Root, determined by OS
-    ///   2. "wallust"
+    /// # Filename structure, magic numbers (cachefmt) after this impl block:
+    ///   1. hash base36 encoded
+    ///   2. CACHE_VER
     ///   3. backend
     ///   4. colorspace
     ///   5. palette
-    ///   6. threshold
-    ///   7. saturation percentage (OPTIONAL)
-    /// # Filename structure:
-    ///   1. hash
-    ///   2. inode number on Linux, file attributes on Windows
-    ///   3. check-contrast -> "C_" if true, "" if false (OPTIONAL)
-    ///   4. fallback generator (if reached)
-    ///   4. [`CACHE_VER`]
+    ///   6. threshold -> "0" means auto, else it's the threshold assign
+    ///   7. saturation percentage -> either it's percentage or "0" if it's disabled
+    ///   8. check-contrast -> 0 off, 1 on
+    ///   9. fallback generator
     pub fn new(file: &Path, c: &Config, cache_path: &Path) -> Result<Self> {
-        // A possible solution to caching a checked/unchecked contrast without cache duplication and
-        // possible efficiency loss
-        // enum Contrast {
-        //     Checked,
-        //     Unchecked,
-        //     UncheckedAndGood,
-        // }
-
-        let sat = if let Some(s) = c.saturation {
-            format!("saturation-{s}")
-        } else {
-            "".to_string()
-        };
-
-        // threshold
-        let th = if c.true_th == 0 { "auto" } else { &c.true_th.to_string() };
-
-        // Cache directory structure
-        let cachepath = Path::new(cache_path)
-            .join("wallust")
-            .join(c.backend.to_string())
-            .join(c.color_space.to_string())
-            .join(c.palette.to_string())
-            .join(th)
-            .join(sat)
-        ;
+        // create cache (e.g. `~/.cache/wallust`)
+        let cachepath = cache_path.join("wallust");
 
         // Create cache dir (with all of it's parents)
-        fs::create_dir_all(&cachepath)?;
+        fs::create_dir_all(&cachepath).with_context(|| "Failed to create {cachepath}")?;
 
-        #[cfg(unix)] // use the ino number on *nix systems
-        let num = fs::metadata(file)?.ino();
+        //XXX maybe later just use an [String; 8].join("_")
 
-        #[cfg(windows)] // and the "magick file number" on windows
-        let num = fs::metadata(file)?.file_attributes();
+        let hash  = base36(fnv1a(&std::fs::read(file)?));
+        let back  = c.backend.cachefmt();
+        let cs    = c.color_space.cachefmt();
+        let palet = c.palette.cachefmt();
+        let th    = if c.true_th == 0 { "0" } else { &c.true_th.to_string() };
+        let sat   = if let Some(s) = c.saturation { s.to_string() } else { "0".to_owned() };
+        let con   = if c.check_contrast.unwrap_or(false) { "1" } else { "0" };
 
-        // Filename structure
-        let basename = format!("{hash}_{magic}_{con}{version}",
-            hash  = fnv1a(&std::fs::read(file)?),
-            magic = num,
-            con   = if c.check_contrast.unwrap_or(false) { "C_" } else { "" },
-            version = CACHE_VER,
-        );
+        let gen = "0"; //asume it didn't reached a fallback generator
+        let basename   = format!("{CACHE_VER}_{back}_{cs}_{palet}_{th}_{sat}_{con}_{gen}_{hash}");
 
-        let gen_letter = match c.fallback_generator.unwrap_or_default() {
-            crate::colorspaces::FallbackGenerator::Interpolate => 'I',
-            crate::colorspaces::FallbackGenerator::Complementary => 'C',
-        };
-
-        let generation = format!("{basename}_{gen_letter}");
+        let gen = c.fallback_generator.unwrap_or_default().cachefmt().to_string(); // real fallback generator
+        let generation = format!("{CACHE_VER}_{hash}_{cs}_{palet}_{th}_{sat}_{con}_{gen}_{hash}");
 
         Ok(Self {
             normal: cachepath.join(basename + ".json"),
             gen:  cachepath.join(generation + ".json"),
             path: PathBuf::new(),
-
         })
     }
 
@@ -132,6 +83,7 @@ impl Cache {
         Ok(serde_json::from_str(&contents)?)
     }
 
+    /// Update path
     pub fn reached_gen(&mut self) {
         self.path.clone_from(&self.gen);
     }
@@ -172,3 +124,102 @@ impl Cache {
     }
 }
 
+/* helpers */
+
+/// Pretty fcking fast hashing
+/// the 32 bit version, should be enough for this use case
+/// Ref: https://en.m.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
+pub fn fnv1a(bytes: &[u8]) -> u32 {
+    let mut hash = 2166136261;
+
+    for byte in bytes {
+        hash ^= *byte as u32;
+        hash = hash.wrapping_mul(16777619);
+    }
+
+    hash
+}
+
+/// simple base36 encoding
+/// Also, there is no need to decode, since it should match if the contents of the file are the
+/// same, else just generate a new scheme.
+/// ref: https://stackoverflow.com/questions/50277050/format-convert-a-number-to-a-string-in-any-base-including-bases-other-than-deci
+pub fn base36(n: u32) -> String {
+    let mut n = n;
+    let mut result = vec![];
+
+    loop {
+        let m = n % 36;
+        n /= 36;
+        result.push(std::char::from_digit(m, 36).expect("is between [2; 36]"));
+        if n == 0 { break; }
+    }
+    result.into_iter().rev().collect()
+}
+
+/* cache fmts, "magic numbers" */
+impl Backend {
+    pub fn cachefmt(&self) -> &str {
+        match self {
+            Backend::Full       => "0",
+            Backend::Resized    => "1",
+            Backend::Wal        => "2",
+            Backend::Thumb      => "3",
+            Backend::FastResize => "4",
+            Backend::Kmeans     => "5",
+        }
+    }
+}
+
+impl ColorSpace {
+    pub fn cachefmt(&self) -> &str {
+        match self {
+            ColorSpace::Lab      => "0",
+            ColorSpace::LabMixed => "1",
+            ColorSpace::Lch      => "2",
+            ColorSpace::LchMixed => "3",
+            ColorSpace::LchAnsi  => "4",
+        }
+    }
+}
+
+
+impl Palette {
+    /// double digits, just to keep a uniform look
+    pub fn cachefmt(&self) -> &str {
+        match self {
+            Palette::Dark            => "00" ,
+            Palette::Dark16          => "01" ,
+            Palette::DarkComp        => "02" ,
+            Palette::DarkComp16      => "03" ,
+            Palette::AnsiDark        => "04" ,
+            Palette::HardDark        => "05" ,
+            Palette::HardDark16      => "06" ,
+            Palette::HardDarkComp    => "07" ,
+            Palette::HardDarkComp16  => "08" ,
+            Palette::Light           => "09" ,
+            Palette::Light16         => "10",
+            Palette::LightComp       => "11",
+            Palette::LightComp16     => "12",
+            Palette::SoftDark        => "13",
+            Palette::SoftDark16      => "14",
+            Palette::SoftDarkComp    => "15",
+            Palette::SoftDarkComp16  => "16",
+            Palette::SoftLight       => "17",
+            Palette::SoftLight16     => "18",
+            Palette::SoftLightComp   => "19",
+            Palette::SoftLightComp16 => "20",
+        }
+    }
+}
+
+impl FallbackGenerator {
+    /// starts from 1, given that 0 means it DIDNT REACHED a fallback generator,
+    /// the scheme generated fine.
+    pub fn cachefmt(&self) -> &str {
+        match self {
+            FallbackGenerator::Interpolate => "1",
+            FallbackGenerator::Complementary => "2",
+        }
+    }
+}
