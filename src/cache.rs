f896c54768428832f0e1ce1a39ee92315c42e58e
explosion-mental <explosion0mental@gmail.com>
2025-05-27 11:46:23 -0500

    cache: bump version, clean up and explain the new logic

--- src/cache.rs
+++ src/cache.rs
@@ -8,10 +8,6 @@ use std::path::PathBuf;
 use palette::Srgb;
 
 use crate::colors::Colors;
-use crate::backends::Backend;
-use crate::palettes::Palette;
-use crate::colorspaces::ColorSpace;
-use crate::colorspaces::FallbackGenerator;
 use crate::config::Config;
 
 use anyhow::{Result, Context};
@@ -20,27 +16,20 @@ use anyhow::{Result, Context};
 /// For example, when there is an internal change in how the
 /// scheme is generated, the cache format won't change, however,
 /// there is a need for a regeneration, so we bump up the version.
-pub const CACHE_VER: &str = "1.6";
+pub const CACHE_VER: &str = "1.7";
 
 /// Used to manage cache, rather than passing arguments in main() a lot
 #[derive(Debug, Default)]
 pub struct Cache {
-    /// The usual naming
-    pub normal: PathBuf,
-    /// naming with when artificially generating colors
-    pub gen: PathBuf,
     /// Path of the cache, this is the path read.
     pub path: PathBuf,
-
+    /// backend file, doesn't include de thereshold since it doesn't affects it
     pub back: PathBuf,
+    /// colorscace file + threshold
     pub cs: PathBuf,
+    /// palette file + threshold
     pub palette: PathBuf,
 }
-
-/// New structure
-/// hash_VERSION -> everything
-/// everything: every method has it's own file.
-
 
 /// Simply print the path when trying to display the [`Cache`] struct
 impl fmt::Display for Cache {
@@ -49,6 +38,7 @@ impl fmt::Display for Cache {
     }
 }
 
+/// Simple shadow for colorscheme return type
 type CSret = (Vec<Srgb>, Vec<Srgb>, bool);
 
 /// Cache order
@@ -62,117 +52,61 @@ pub enum IsCached {
 
 impl Cache {
     /// # Filename structure, magic numbers (cachefmt) after this impl block:
-    ///   1. hash base36 encoded
-    ///   2. CACHE_VER
-    ///   3. backend
-    ///   4. colorspace
-    ///   5. palette
-    ///   6. threshold -> "0" means auto, else it's the threshold assign
-    ///   7. saturation percentage -> either it's percentage or "0" if it's disabled
-    ///   8. check-contrast -> 0 off, 1 on
-    ///   9. fallback generator
+    /// *Each hash image has it's own dir*, inside there is multiple files:
+    /// 1. Backend file, with the full name, maybe reductant with the `full` backend.
+    /// 2. ColorSpace + threshold, since it depends on the threshold
+    /// 3. Scheme + ColorSpace + threshold, since the palette depends on the colorspace, and the colorspace on the threshold
+    /// This new structure allows you to reuse some parts, when configuring, avoding more time waiting.
     pub fn new(file: &Path, c: &Config, cache_path: &Path) -> Result<Self> {
         // create cache (e.g. `~/.cache/wallust`)
         let cachepath = cache_path.join("wallust");
 
-        // Create cache dir (with all of it's parents)
-
-        //XXX maybe later just use an [String; 8].join("_")
-
+        // hash value for the file, since you can duplicate it, but the contents are the same.
         let hash  = base36(fnv1a(&std::fs::read(file)?));
 
+        // Create cache dir (with all of it's parents)
         fs::create_dir_all(&cachepath.join(format!("{hash}_{CACHE_VER}"))).with_context(|| "Failed to create {cachepath}")?;
 
-        let back  = c.backend.cachefmt();
-        let cs    = c.color_space.cachefmt();
-        let palet = c.palette.cachefmt();
         let th    = if c.true_th == 0 { "auto" } else { &c.true_th.to_string() };
-        let sat   = if let Some(s) = c.saturation { s.to_string() } else { "0".to_owned() };
-        let con   = if c.check_contrast.unwrap_or(false) { "1" } else { "0" };
+        // wallust/image_1.0/
+        let base = cachepath.join(format!("{hash}_{CACHE_VER}"));
 
-        let gen = "0"; //asume it didn't reached a fallback generator
-        let basename   = format!("{CACHE_VER}_{back}_{cs}_{palet}_{th}_{sat}_{con}_{gen}_{hash}");
-
-        let gen = c.fallback_generator.unwrap_or_default().cachefmt().to_string(); // real fallback generator
-        let generation = format!("{CACHE_VER}_{hash}_{cs}_{palet}_{th}_{sat}_{con}_{gen}_{hash}");
-
-        let start = cachepath.join(format!("{hash}_{CACHE_VER}"));
-
-
-        let bk = c.backend.to_string();
-        let col = c.color_space.to_string();
-        let pal = c.palette.to_string();
-
-        // std::fs::create_dir(&start)?;
+        let back = c.backend.to_string();
+        let cs  = c.color_space.to_string();
+        let palet = c.palette.to_string();
 
         Ok(Self {
-            normal: cachepath.join(basename + ".json"),
-            gen:  cachepath.join(generation + ".json"),
-            path: PathBuf::new(),
-            back: start.join(format!("{bk}")),
-            cs: start.join(format!("{col}_{th}")),
-            palette: start.join(format!("{pal}_{th}")),
+            path: cachepath,
+            back: base.join(format!("{back}")),
+            cs: base.join(format!("{cs}_{th}")),
+            palette: base.join(format!("{cs}_{th}_{palet}")),
         })
     }
 
-    /// Fetches values from a file present in cache
-    pub fn read(&self) -> Result<Colors> {
-        let contents = std::fs::read_to_string(&self.path)?;
-        Ok(serde_json::from_str(&contents)?)
-    }
-
-    /// Update path
-    pub fn reached_gen(&mut self) {
-        self.path.clone_from(&self.gen);
-    }
-
-    /// Write values to cache
-    pub fn write(&self, colors: &Colors) -> Result<()> {
-        Ok(File::create(&self.path)?
-            .write_all(
-                serde_json::to_string_pretty(colors)
-                    .with_context(|| format!("Failed to deserilize from the json cached file: '{}':", &self))?
-                .as_bytes()
-            )?
-        )
-    }
+    // Update path
+    // pub fn reached_gen(&mut self) {
+    //     self.path.clone_from(&self.gen);
+    // }
 
     pub fn read_backend(&self) -> Result<Vec<u8>> {
-        // if self.back.exists() {
-            let contents = std::fs::read_to_string(&self.back)?;
-            let v: Vec<u8> = serde_json::from_str(&contents)?;
-            Ok(v)
-        // } else {
-        //     Ok(None)
-        // }
+        let contents = std::fs::read_to_string(&self.back)?;
+        let v: Vec<u8> = serde_json::from_str(&contents)?;
+        Ok(v)
     }
 
     pub fn read_cs(&self) -> Result<CSret> {
-        // if self.cs.exists() {
-            let contents = std::fs::read_to_string(&self.cs)?;
-            let v: CSret = serde_json::from_str(&contents)?;
-            Ok(v)
-        // } else {
-        //     Ok(None)
-        // }
+        let contents = std::fs::read_to_string(&self.cs)?;
+        let v: CSret = serde_json::from_str(&contents)?;
+        Ok(v)
     }
 
     pub fn read_palette(&self) -> Result<Colors> {
-        // println!("helloone!");
-        // if self.palette.exists() {
-            // println!("Exist!");
-            let contents = std::fs::read_to_string(&self.palette)?;
-            // println!("readd!");
-            let v: Colors = serde_json::from_str(&contents)?;
-            // println!("ond!");
-            Ok(v)
-        // } else {
-        //     Ok(None)
-        // }
+        let contents = std::fs::read_to_string(&self.palette)?;
+        let v: Colors = serde_json::from_str(&contents)?;
+        Ok(v)
     }
 
     pub fn write_backend(&self, bytes: &[u8]) -> Result<()> {
-        // println!("{:?}", self.back);
         Ok(File::create(&self.back)?
             .write_all(
                 serde_json::to_string(bytes)
@@ -217,30 +151,6 @@ impl Cache {
             IsCached::None
         }
     }
-
-    /// To determine whether to read from cache or to generate the colors from scratch
-    /// If not found, check if the generated path exist, it could may be that it doesn't have
-    /// enought colors.
-    pub fn is_cached(&mut self) -> bool {
-        let normal = self.normal.exists();
-        let gen = self.gen.exists();
-
-        let (new_path, ret) = match (normal, gen) {
-            //some exist, so `is_cached()` true
-            (true, false) => (&self.normal, true),
-            (false, true) => (&self.gen, true),
-
-            // none cached, default to normal and `is_cached()` false
-            (false, false) => (&self.normal, false),
-
-            // unusual (imposible?) case. Just default to normal path.
-            // if the code reaches the generation part, `reached_gen()` should be called anyway.
-            (true,  true)   => (&self.normal, true),
-        };
-
-        self.path.clone_from(new_path);
-        ret
-    }
 }
 
 /* helpers */
@@ -274,73 +184,4 @@ pub fn base36(n: u32) -> String {
         if n == 0 { break; }
     }
     result.into_iter().rev().collect()
-}
-
-/* cache fmts, "magic numbers" */
-impl Backend {
-    pub fn cachefmt(&self) -> &str {
-        match self {
-            Backend::Full       => "0",
-            Backend::Resized    => "1",
-            Backend::Wal        => "2",
-            Backend::Thumb      => "3",
-            Backend::FastResize => "4",
-            Backend::Kmeans     => "5",
-        }
-    }
-}
-
-impl ColorSpace {
-    pub fn cachefmt(&self) -> &str {
-        match self {
-            ColorSpace::Lab      => "0",
-            ColorSpace::LabMixed => "1",
-            ColorSpace::Lch      => "2",
-            ColorSpace::LchMixed => "3",
-            ColorSpace::LchAnsi  => "4",
-        }
-    }
-}
-
-
-impl Palette {
-    /// double digits, just to keep a uniform look
-    /// since now, all newly added palettes shall be appended.
-    pub fn cachefmt(&self) -> &str {
-        match self {
-            Palette::Dark            => "00" ,
-            Palette::Dark16          => "01" ,
-            Palette::DarkComp        => "02" ,
-            Palette::DarkComp16      => "03" ,
-            Palette::AnsiDark        => "04" ,
-            Palette::HardDark        => "05" ,
-            Palette::HardDark16      => "06" ,
-            Palette::HardDarkComp    => "07" ,
-            Palette::HardDarkComp16  => "08" ,
-            Palette::Light           => "09" ,
-            Palette::Light16         => "10",
-            Palette::LightComp       => "11",
-            Palette::LightComp16     => "12",
-            Palette::SoftDark        => "13",
-            Palette::SoftDark16      => "14",
-            Palette::SoftDarkComp    => "15",
-            Palette::SoftDarkComp16  => "16",
-            Palette::SoftLight       => "17",
-            Palette::SoftLight16     => "18",
-            Palette::SoftLightComp   => "19",
-            Palette::SoftLightComp16 => "20",
-            Palette::AnsiDark16      => "21" ,
-        }
-    }
-}
-
-impl FallbackGenerator {
-    /// starts from 1, given that 0 means it DIDNT REACHED a fallback generator,
-    /// the scheme generated fine.
-    pub fn cachefmt(&self) -> &str {
-        match self {
-            FallbackGenerator::Interpolate => "1",
-            FallbackGenerator::Complementary => "2",
-        }
-    }
 }
